b123ba6e2b0db56ced35d55fee2aebdaf2bff477
Boil Britt <boilbrit@il-e2.msk.21-school.ru>
2024-04-17 19:44:20 +0300

    add conv and parsers funcs

--- src/4_Convertation_and_parsers/s21_from_decimal_to_float.c
+++ src/4_Convertation_and_parsers/s21_from_decimal_to_float.c
@@ -1,1 +1,3 @@
-#include "../include/s21_decimal.h"
+#include "./../6_Headers/s21_convertation_and_parsers.h"
+
+int s21_from_decimal_to_float(s21_decimal src, float *dst);
--- src/4_Convertation_and_parsers/s21_from_float_to_decimal.c
+++ src/4_Convertation_and_parsers/s21_from_float_to_decimal.c
@@ -1,1 +1,18 @@
-#include "../include/s21_decimal.h"
+#include "./../6_Headers/s21_convertation_and_parsers.h"
+
+// From float
+int s21_from_float_to_decimal(float src, s21_decimal *dst) {
+  int flag_error = S21_SUCCES;
+  if (dst) {
+    if (src < 0) {
+      src *= -1;
+      *dst = s21_decimal_set_bit(*dst, SIGN_POS, 1);
+    }
+    int quotient = (int)src;
+    int fraction_part = src - quotient;
+    int exp = 0;
+  } else {
+    flag_error = S21_CONV_ERROR;
+  }
+  return flag_error;
+}
