5512b69b221cafba0fee9ffca3723330d8a93bf1
Babs Star <babsstar@mi-h3.msk.21-school.ru>
2024-04-22 21:09:04 +0300

    push3

--- 5_Anouther_function/s21_negate.c
+++ 5_Anouther_function/s21_negate.c
@@ -1,12 +1,7 @@
 #include "./../6_Headers/s21_decimal.h"
 
 // Returns the result of multiplying the specified Decimal value by negative one
-// int s21_get_sign(s21_decimal value) {
-//   return (value.bits[3] & 0x80000000) >> 31;
-// }
-// int s21_get_exp(s21_decimal value) {
-//   return (value.bits[3] & 0x00FF0000) >> 16;
-// }
+
 int s21_negate(s21_decimal value, s21_decimal *result) {
   *result = value;
   result->bits[3] =
