0a170d2c88697fd875454b483ed9b4d7f7ff2ac8
German Muraev <german_muraev@mail.ru>
2024-05-01 16:43:02 +0300

    WIP

--- src/test/test_s21_decimal.h
+++ src/test/test_s21_decimal.h
@@ -4,6 +4,9 @@
  
 // добавляем все тесты для s21_is_equal в один набор
 Suite *test_s21_is_equal_suite();
+Suite *test_s21_is_not_equal_suite();
+Suite *test_s21_is_less_suite();
+Suite *test_s21_is_less_or_equal_suite();
 
 // функция для прогона набора тестов
 unsigned int run_suite(Suite *suite, char *file_logs);
--- src/test/test_s21_is_not_equal.c
+++ src/test/test_s21_is_not_equal.c
@@ -6,8 +6,8 @@ START_TEST(test_s21_is_not_equal_1) {
     s21_decimal_init(&decimal_1);
     s21_decimal_init(&decimal_2);
 
-    int result_s21_is_equal = s21_is_equal(decimal_1, decimal_2);
-    ck_assert_int_eq(result_s21_is_equal, S21_FALSE);
+    int result_s21_is_not_equal = s21_is_not_equal(decimal_1, decimal_2);
+    ck_assert_int_eq(result_s21_is_not_equal, S21_FALSE);
 }
 END_TEST
 
@@ -18,44 +18,46 @@ START_TEST(test_s21_is_not_equal_2) {
     s21_decimal_init(&decimal_2);
     s21_decimal_set_bit(decimal_1, 10, 1);
 
-    int result_s21_is_equal = s21_is_equal(decimal_1, decimal_2);
-    ck_assert_int_eq(result_s21_is_equal, S21_TRUE);
+    int result_s21_is_not_equal = s21_is_not_equal(decimal_1, decimal_2);
+    ck_assert_int_eq(result_s21_is_not_equal, S21_TRUE);
 }
 END_TEST
 
 START_TEST(test_s21_is_not_equal_3) {
-    // тест 2
+    // тест 3
     s21_decimal decimal_1, decimal_2;
     s21_decimal_init(&decimal_1);
     s21_decimal_init(&decimal_2);
-    s21_decimal_set_bit(decimal_1, 10, 1);
+    s21_decimal_set_bit(decimal_1, 127, 1);
 
-    int result_s21_is_equal = s21_is_equal(decimal_1, decimal_2);
-    ck_assert_int_eq(result_s21_is_equal, S21_FALSE);
+    int result_s21_is_not_equal = s21_is_not_equal(decimal_1, decimal_2);
+    ck_assert_int_eq(result_s21_is_not_equal, S21_FALSE);
 }
 END_TEST
 
 START_TEST(test_s21_is_not_equal_4) {
-    // тест 2
+    // тест 4
     s21_decimal decimal_1, decimal_2;
     s21_decimal_init(&decimal_1);
     s21_decimal_init(&decimal_2);
-    s21_decimal_set_bit(decimal_1, 10, 1);
+    s21_decimal_set_bit(decimal_2, 95, 1);
 
-    int result_s21_is_equal = s21_is_equal(decimal_1, decimal_2);
-    ck_assert_int_eq(result_s21_is_equal, S21_FALSE);
+    int result_s21_is_not_equal = s21_is_not_equal(decimal_1, decimal_2);
+    ck_assert_int_eq(result_s21_is_not_equal, S21_TRUE);
 }
 END_TEST
 
 START_TEST(test_s21_is_not_equal_5) {
-    // тест 2
+    // тест 5
     s21_decimal decimal_1, decimal_2;
     s21_decimal_init(&decimal_1);
     s21_decimal_init(&decimal_2);
     s21_decimal_set_bit(decimal_1, 10, 1);
+    s21_decimal_set_bit(decimal_2, 10, 1);
+    s21_decimal_set_bit(decimal_2, 127, 1);
 
-    int result_s21_is_equal = s21_is_equal(decimal_1, decimal_2);
-    ck_assert_int_eq(result_s21_is_equal, S21_FALSE);
+    int result_s21_is_not_equal = s21_is_not_equal(decimal_1, decimal_2);
+    ck_assert_int_eq(result_s21_is_not_equal, S21_TRUE);
 }
 END_TEST
 
@@ -67,9 +69,10 @@ Suite *test_s21_is_not_equal_suite() {
     // каждый тест добавляем
     tcase_add_test(tc_core, test_s21_is_not_equal_1);
     tcase_add_test(tc_core, test_s21_is_not_equal_2);
-
+    tcase_add_test(tc_core, test_s21_is_not_equal_3);
+    tcase_add_test(tc_core, test_s21_is_not_equal_4);
+    tcase_add_test(tc_core, test_s21_is_not_equal_5);
     // тест кейс добавляем в suite
     suite_add_tcase(suite, tc_core);
-
     return suite;
 }
