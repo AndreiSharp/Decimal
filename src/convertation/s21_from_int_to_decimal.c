e2cba90f575b136b3eddbba0e94143b0bee71803
funch0za <nadya.sm101@yandex.ru>
2024-05-08 14:08:33 +0300

    fix bugs in s21_from_int_to_decimal

--- src/convertation/s21_from_int_to_decimal.c
+++ src/convertation/s21_from_int_to_decimal.c
@@ -1,11 +1,11 @@
 #include "../include/s21_decimal.h"
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
-  int flag = S21_SUCCES;
+  int flag = S21_CONV_SUCCESS;
   if (dst) {
     if (src < 0) {
       src *= -1;
-      s21_decimal_set_bit(dst, MAX_BIT_BLOCKS, 1);
+      *dst = s21_decimal_set_bit(*dst, DECIMAL_SIGN_POS, 1);
     }
     dst->bits[0] = src;
   } else {
