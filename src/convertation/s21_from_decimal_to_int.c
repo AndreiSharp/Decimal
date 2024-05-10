77f59a20afd8204d1ffb972470c506f76bcfeef9
funch0za <nadya.sm101@yandex.ru>
2024-05-10 22:21:51 +0300

    VIP: convertations work

--- src/convertation/s21_from_decimal_to_int.c
+++ src/convertation/s21_from_decimal_to_int.c
@@ -1,17 +1,16 @@
 #include "../include/s21_decimal.h"
 
-/*
 int s21_from_decimal_to_int(s21_decimal src, int *dst) {
   int flag = S21_SUCCES;
   if (dst) {
     int index_low_bit = s21_decimal_exp(src);
-    int index_high_bit = s21_decimal_is_set_bit(src, SIZE_BLOCK);
+    int index_high_bit = s21_decimal_is_set_bit(src, SIZE_MANTIS);
     int size_int_mantis = index_high_bit - index_low_bit;
     if ((size_t) size_int_mantis > sizeof(int)) {
       while (size_int_mantis >= 0) {
-        (*dst) << size_int_mantis;
+        (*dst) <<= size_int_mantis;
         (*dst) &= s21_decimal_is_set_bit(src, size_int_mantis);
-        (*dst) >> size_int_mantis;
+        (*dst) >>= size_int_mantis;
         size_int_mantis--;
       }
       if (s21_decimal_sign(src)) {
@@ -23,5 +22,6 @@ int s21_from_decimal_to_int(s21_decimal src, int *dst) {
   } else {
     flag = S21_CONV_ERROR;
   }
+
+  return flag;
 }
-*/
--- src/convertation/s21_from_float_to_decimal.c
+++ src/convertation/s21_from_float_to_decimal.c
@@ -1,20 +1,25 @@
 #include "../include/s21_decimal.h"
 
-// From float
-/*
 int s21_from_float_to_decimal(float src, s21_decimal *dst) {
-  int flag_error = S21_SUCCES;
-  if (dst) {
-    if (src < 0) {
-      src *= -1;
-      *dst = s21_decimal_set_bit(*dst, DECIMAL_SIGN_POS, 1);
-    }
-    int quotient = (int)src;
-    int fraction_part = src - quotient;
-    int exp = 0;
-  } else {
-    flag_error = S21_CONV_ERROR;
-  }
-  return flag_error;
+	unsigned int error_code = S21_CONV_SUCCESS;
+	if (dst) {
+		unsigned int exp = 0, sign = 1;
+		if (src < 0) {
+			sign = 0;
+			src *= -1;
+		}
+		int src_int = (int) src;
+		while (src - src_int > 0) {
+			src *= 10;
+			src_int = (int) src;
+			++exp;
+		}
+		s21_from_int_to_decimal(src_int, dst); 
+		*dst = s21_decimal_set_exp(*dst, exp);
+		*dst = s21_decimal_set_bit(*dst, DECIMAL_SIGN_POS, sign);
+	} else {
+		error_code = S21_CONV_ERROR;
+	}
+
+	return error_code;
 }
-*/
