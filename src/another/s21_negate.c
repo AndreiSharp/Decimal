d8d5ac9f3350d7967a3e86286794d944b329f880
Semen Bushmanov <bush.sem@mail.ru>
2024-05-12 21:55:21 +0300

    Add negate function and tests for it

--- src/another/s21_negate.c
+++ src/another/s21_negate.c
@@ -1,4 +1,16 @@
 #include "../include/s21_decimal.h"
 
-// Returns the result of multiplying the specified Decimal value by negative one
-int s21_negate(s21_decimal value, s21_decimal *result) {}
+/*
+  @brief Returns the result of multiplying the specified Decimal value by negative one
+  @rosalori
+  @param value decimal
+  @param result decimal
+  @return flag mistake
+ */
+
+int s21_negate(s21_decimal value, s21_decimal *result) {
+  s21_decimal negative_one = {{1, 0, 0, 0b10000000000000000000000000000000}};
+  int error_status = s21_mul(value, negative_one, result);
+
+  return error_status;
+}
