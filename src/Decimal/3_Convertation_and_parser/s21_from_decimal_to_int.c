b50aefdb88f1fb232e57e038cdf656c53347097b
Boil Britt <boilbrit@il-c4.msk.21-school.ru>
2024-05-21 21:56:09 +0300

    decimal to int

--- src/Decimal/3_Convertation_and_parser/s21_from_decimal_to_int.c
+++ src/Decimal/3_Convertation_and_parser/s21_from_decimal_to_int.c
@@ -1,42 +1,20 @@
 #include "./../../Headers/s21_decimal.h"
+#include <stdint.h>
 
 int s21_from_decimal_to_int(s21_decimal src, int *dst) {
   s21_DecData src_DecData = s21_decimal_get_data(src);
 
   int error_code = S21_CONV_SUCCESS;
   if (dst && s21_decimal_is_correct(src)) {
-    bit32_t size_int_mantis = src_DecData.high_bit;
-    if ((size_t) size_int_mantis > sizeof(int)) {
-      while (size_int_mantis >= 0) {
-        (*dst) <<= size_int_mantis;
-        (*dst) &= s21_decimal_get_bit(src_DecData.value, size_int_mantis);
-        (*dst) >>= size_int_mantis;
-        size_int_mantis--;
-      }
-      if (src_DecData.sign == 1) {
-        (*dst) *= -1;
-      }
-    } else {
-      error_code = S21_CONV_ERROR;
-    }
-  } else {
-    error_code = S21_CONV_ERROR;
-  }
-
-  return error_code;
-}
-
-int funch0za_s21_from_decimal_to_int(s21_decimal src, int *dst) {
-  s21_DecData src_DecData = s21_decimal_get_data(src);
-
-  int error_code = S21_CONV_SUCCESS;
-  if (dst && s21_decimal_is_correct(src)) {
     s21_count_div_10(&src_DecData, src_DecData.scale);
-
-    if ((size_t) src_DecData.high_bit > sizeof(int)) {
-      
+ 
+    if ((size_t) src_DecData.high_bit > sizeof(int) || src_DecData.value.bits[0] > INT32_MAX) {
+      error_code = S21_CONV_ERROR;
+    } else {
+      *dst = src_DecData.value.bits[0];
     }
   } else {
     error_code = S21_CONV_ERROR;
   }
+  return error_code;
 }
