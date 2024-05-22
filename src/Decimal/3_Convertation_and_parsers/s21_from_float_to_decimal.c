b21fbaa4853744d497eb60713537ef24d24e3473
Mr_Sharpear <andreyostrouhov@Morsbardrock.local>
2024-05-22 23:00:57 +0300

    fix conv&pars

--- src/Decimal/3_Convertation_and_parsers/s21_from_float_to_decimal.c
+++ src/Decimal/3_Convertation_and_parsers/s21_from_float_to_decimal.c
@@ -1,5 +1,5 @@
 #include "./../../Headers/s21_convertation_and_parsers.h"
-// переполнение инта
+
 int s21_from_float_to_decimal(float src, s21_decimal *dst) {
   s21_DecData dst_DecData = s21_decimal_null_data();
   unsigned int error_code = S21_CONV_SUCCESS;
@@ -8,12 +8,20 @@ int s21_from_float_to_decimal(float src, s21_decimal *dst) {
       dst_DecData.sign = 1;
       src *= -1;
     }
-    for (int i = 0; i < FLOAT_LEN; ++i) {
+    int i = 0;
+    for (i; i < FLOAT_LEN; ++i) {
       src *= 10;
     }
-    s21_from_int_to_decimal((int)src, &dst_DecData.value);
+    dst_DecData.scale = i;
+    error_code = s21_from_int_to_decimal((int)src, &dst_DecData.value);
+    if (!error_code) {
+      *dst = s21_decimal_set_data(dst_DecData);
+    } else {
+      *dst = s21_decimal_null();
+    }
   } else {
     error_code = S21_CONV_ERROR;
+    *dst = s21_decimal_null();
   }
   return error_code;
 }
