fe393f7d860ab14db05510455e434b671c6f4d60
Mr_Sharpear <andreyostrouhov@Morsbardrock.local>
2024-05-24 19:00:40 +0300

    fix trunc

--- src/Decimal/4_Anouther_function/s21_truncate.c
+++ src/Decimal/4_Anouther_function/s21_truncate.c
@@ -4,9 +4,13 @@ int s21_truncate(s21_decimal value, s21_decimal *result) {
   int flag = 0;  //// добавить ошибки при вычислении (если переменные не по
                  /// формату и тп)
   s21_DecData val = s21_decimal_get_data(value);
-  for (int i = val.scale; i > 0; i--) {
-    s21_div_10(&val);
+  if (val.high_bit - 3 * val.scale >= 0) {
+    for (int i = val.scale; i > 0; i--) {
+      s21_div_10(&val);
+    }
+    *result = s21_decimal_set_data(val);
+  } else {
+    *result = s21_decimal_null();
   }
-  *result = s21_decimal_set_data(val);
   return flag;
 }
