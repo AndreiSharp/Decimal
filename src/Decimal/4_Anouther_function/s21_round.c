8204289421f0280b060193dd90573c2f4cbbfeac
Mr_Sharpear <andreyostrouhov@Morsbardrock.local>
2024-05-25 16:28:13 +0300

    fix bug floor truncate round

--- src/Decimal/4_Anouther_function/s21_round.c
+++ src/Decimal/4_Anouther_function/s21_round.c
@@ -3,16 +3,16 @@
 int s21_round(s21_decimal value, s21_decimal *result) {
   bit32_t error_code = S21_ANFUNC_SUCCESS;
   error_code = s21_truncate(value, result);
-  s21_DecData val = s21_decimal_get_data(value);
   if (!error_code) {
+    s21_DecData val = s21_decimal_get_data(value);
     if (val.scale) {
       s21_DecData res = s21_decimal_get_data(*result);
+      res.sign = val.sign;
       s21_count_mul_10(&res, val.scale);
       s21_sub_mantis(val, res, &val);
       s21_count_div_10(&res, val.scale);
       s21_count_div_10(&val, val.scale - 1);
-      bit32_t comp_result = s21_compare_residue_10(val);
-      if (comp_result != 2) {
+      if (s21_compare_residue_10(val) != 2) {
         error_code = s21_decimal_add_one(res, &res);
       }
       if (!error_code) {
--- src/Decimal/4_Anouther_function/s21_truncate.c
+++ src/Decimal/4_Anouther_function/s21_truncate.c
@@ -1,14 +1,19 @@
 #include "./../../Headers/s21_another_function.h"
 
 int s21_truncate(s21_decimal value, s21_decimal *result) {
-  int flag = 0;  //// добавить ошибки при вычислении (если переменные не по
-                 /// формату и тп)
+  int flag = S21_ANFUNC_SUCCESS;  //// добавить ошибки при вычислении (если
+                                  ///переменные не по
+                                  /// формату и тп)
   s21_DecData val = s21_decimal_get_data(value);
+  s21_DecData res = s21_decimal_null_data();
+  res.sign = val.sign;
   if ((int)val.high_bit - 3 * val.scale >= 0) {
     for (int i = val.scale; i > 0; i--) {
       s21_div_10(&val);
     }
-    *result = s21_decimal_set_data(val);
+    res.value = val.value;
+    res.high_bit = val.high_bit;
+    *result = s21_decimal_set_data(res);
   } else {
     *result = s21_decimal_null();
   }
