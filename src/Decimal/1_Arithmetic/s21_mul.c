84ccb11956493d1079e2dde5ad23b7bc3edc8094
Mr_Sharpear <andreyostrouhov@Morsbardrock.local>
2024-05-26 16:42:05 +0300

    fix rul for null decimal

--- src/Decimal/1_Arithmetic/s21_mul.c
+++ src/Decimal/1_Arithmetic/s21_mul.c
@@ -3,7 +3,7 @@
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
   bit32_t code_error = S21_SUCCES;
 
-  if (s21_decimal_is_null(value_1) && s21_decimal_is_null(value_2)) {
+  if (s21_decimal_is_null(value_1) || s21_decimal_is_null(value_2)) {
     *result = s21_decimal_null();
   } else {
     s21_DecData val_data_1 = s21_decimal_get_data(value_1);
