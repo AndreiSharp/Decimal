e583d4f6e7e647362492f90211c13eb2659da92d
Mr_Sharpear <andreyostrouhov@Morsbardrock.local>
2024-05-28 18:16:59 +0300

    fix from float to dec

--- src/Decimal/3_Convertation_and_parsers/s21_from_float_to_decimal.c
+++ src/Decimal/3_Convertation_and_parsers/s21_from_float_to_decimal.c
@@ -1,44 +1,43 @@
+#include <math.h>
+
 #include "./../../Headers/s21_convertation_and_parsers.h"
 
-char s21_is_not_max_float(float x) {
-  return (MIN_FLOAT_TO_CONVERT < fabs(x) && fabs(x) < MAX_FLOAT_TO_CONVERT) || x == 0.0;
-}
-
 int s21_from_float_to_decimal(float src, s21_decimal *dst) {
-  s21_DecData dst_DecData = s21_decimal_null_data();
   bit32_t error_code = S21_CONV_SUCCESS;
-  if (dst && !isinf(src) && !isnan(src) && s21_is_not_max_float(src)) {
-    if (src < 0) {
-      dst_DecData.sign = 1;
-      src = fabs(src);
-    }
-    bit32_t len_int_part_from_src = 0;
-    int src_int = src;
-    printf("float %f, int part %d\n", src, src_int);
-    do {
-      src_int /= 10;
-      ++len_int_part_from_src;
-    }while (src_int > 0);
-    printf("len int part %u count mul %u\n", len_int_part_from_src, FLOAT_LEN - len_int_part_from_src);
-    for (int i = len_int_part_from_src; i < FLOAT_LEN; ++i) {
-      src *= 10;
-      printf("float %f\n", src);
-      dst_DecData.scale++;
-    }
-
-    error_code = s21_from_int_to_decimal(round(src), &dst_DecData.value);
-    if (error_code == S21_CONV_SUCCESS) {
-      printf("src : %f, round(src) : %f\n", src, round(src));
-      dst_DecData.high_bit = s21_decimal_get_high_bit(dst_DecData.value);
-      *dst = s21_decimal_set_data(dst_DecData); 
-    } else {
-      *dst = s21_decimal_null();
-    }
-  } else {
+  if (dst == NULL || isinf(src) || isnan(src)) {
     error_code = S21_CONV_ERROR;
+  } else if (fabsf(src) > MAX_FLOAT_TO_CONVERT) {
+    error_code = S21_CONV_ERROR;
+  } else if (fabsf(src) == 0.0) {
     *dst = s21_decimal_null();
-  }
+  } else if (fabsf(src) < MIN_FLOAT_TO_CONVERT) {
+    *dst = s21_decimal_null();
+    error_code = S21_CONV_ERROR;
+  } else {
+    float new = src;
+    *dst = s21_decimal_null();
+    if (new < 0.0) {
+      *dst = s21_decimal_set_sign(*dst, 1);
+      src = fabsf(src);
+    }
+    float check_scale = src;
+    int scale = 7;
+    while (check_scale > 1) {
+      check_scale /= 10;
+      scale--;
+    }
+    if (scale > 0) {
+      for (int i = scale; i > 0; i--) {
+        src *= 10;
+      }
+      float new = src - round(src * 10) / 10;
+      if (new > 1e-7) {
+        src++;
+      }
+    }
 
-  puts("");
+    dst->bits[0] = round(src);
+    *dst = s21_decimal_set_scale(*dst, scale);
+  }
   return error_code;
 }
